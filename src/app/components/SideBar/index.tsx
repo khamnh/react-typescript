cdafa43620bfcf2935ef80d330a477cdc48d2d76
preddevil213 <pnguyen@newzik.com>
2018-08-03 16:15:48 +0700

    Fix TSlint and add SASS to project

--- src/app/components/SideBar/index.tsx
+++ src/app/components/SideBar/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import './style.css';
+import './style.scss';
 
 export class SideBar extends React.Component {
     render(): React.ReactNode {
--- src/app/containers/App/index.tsx
+++ src/app/containers/App/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import './style.css';
+import './style.scss';
 import { RouteComponentProps, Route, Switch } from 'react-router-dom';
 import Contract from '../Contracts';
 import Library from '../Library';

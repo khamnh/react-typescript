e676997101d94af85e9b703f6329f490d7699a0a
hungtran210 <htran@bocasay.com>
2018-08-04 13:25:55 +0700

    build production by webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,4 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.tsx',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(scss|sass)$/,
-        use: [{
-          loader: 'style-loader',
-        }, {
-          loader: 'css-loader',
-        }, {
-          loader: 'sass-loader',
-          options: {
-            includePaths: ['./app/styles'],
-          },
-        }],
-      },
-    ]
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/public/index.html',
-    })
-  ],
+module.exports = function (env = 'dev') {
+  return require(`./webpack.${env}.js`);
 };
